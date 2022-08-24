32d612e9c3a7e303211db3365131ddfebea4935c
Hilman Mamonto <hmamonto27@gmail.com>
2022-08-24 13:17:36 +0800

    update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,36 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { createContext, useReducer } from "react";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+interface InitialState {
+  name: string;
+  age: number;
 }
 
-export default MyApp
+const initialState = {
+  name: "",
+  age: 0,
+};
+
+export const RootContext = createContext({});
+const Provider = RootContext.Provider;
+
+function reducer(action: any, state: InitialState) {
+  switch (action.type) {
+    case "UPDATE_AGE":
+      return { ...state, age: state.age + 1 };
+    default:
+      break;
+  }
+}
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <Provider value={{ state, dispatch }}>
+      <Component {...pageProps} />
+    </Provider>
+  );
+}
+
+export default MyApp;
