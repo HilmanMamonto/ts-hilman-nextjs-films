4ab9f78a320d502d206b8e303538c67f6cdb5873
Hilman Mamonto <hmamonto27@gmail.com>
2022-08-24 22:55:29 +0800

    Fix useReducer
    in reducer function must return the state

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,31 +1,33 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { createContext, useReducer } from "react";
 
-interface InitialState {
-  name: string;
+type InitialState = {
   age: number;
-}
+};
 
-const initialState = {
-  name: "",
+type Action = {
+  type: string;
+};
+
+const initialState: InitialState = {
   age: 0,
 };
 
-export const RootContext = createContext({});
-const Provider = RootContext.Provider;
-
-function reducer(action: any, state: InitialState) {
+function reducer(state: InitialState, action: Action) {
   switch (action.type) {
     case "UPDATE_AGE":
-      return { ...state, age: state.age + 1 };
+      return { age: state.age };
     default:
-      break;
+      return state;
   }
 }
 
+const RootContext = createContext({});
+const Provider = RootContext.Provider;
+
 function MyApp({ Component, pageProps }: AppProps) {
   const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <Provider value={{ state, dispatch }}>
       <Component {...pageProps} />
